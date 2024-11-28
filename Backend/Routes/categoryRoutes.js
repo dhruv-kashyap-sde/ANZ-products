6a3dff9d343d287eb459fbf1d9a401bea9202771
Dhruv Kashyap <dhruv.kashyap1111@gmail.com>
2024-11-28 23:33:10 +0530

    added controller and routes but needed some changes

---
+++ Backend/Routes/categoryRoutes.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+// const router = express.Router();
+// const categoryController = require('../controllers/categoryController');
+
+// // Create a new category
+// router.post('/categories', categoryController.createCategory);
+
+// // Add a product to a category
+// router.post('/products', categoryController.addProductToCategory);
+
+// // Get all categories
+// router.get('/categories', categoryController.getAllCategories);
+
+// module.exports = router;
