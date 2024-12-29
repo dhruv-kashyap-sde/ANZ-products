14e83e4a3cafbbab5149186071b3412a5e0173a5
Dhruv Kashyap <dhruv.kashyap1111@gmail.com>
2024-12-29 14:31:25 +0530

    integrate Cloudinary for product image uploads and remove multer from route

--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const upload = require('../config/multerConfig');
 
 // Add a product with file upload
-router.post('/add-products', upload.single('image'), productController.addProduct);
+router.post('/add-products', productController.addProduct);
 
 // Get all products
 router.get('/get-all-products', productController.getAllProducts);
